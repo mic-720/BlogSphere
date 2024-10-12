b363d46bda8e4a18a1c19a677d71d228f2784cf1
mic-720 <satyamksvc@gmail.com>
2024-10-12 18:25:06 +0530

    updates index.js

--- schema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-
-module.exports.blogSchema = Joi.object({
-  title: Joi.string().required(),
-  content: Joi.string().required(),
-  category: Joi.string().required(),
-  tags: Joi.array().items(Joi.string()).required(), // Array of strings for tags
-});
-
